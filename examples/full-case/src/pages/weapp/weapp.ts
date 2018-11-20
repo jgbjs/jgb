76f137cb3d7f5fb4817727a48bb593e289b26dc6
蒋瑶 <landn172@gmail.com>
2018-11-20 15:45:42 +0800

    test: update example

--- examples/full-case/src/pages/weapp/weapp.ts
+++ examples/full-case/src/pages/weapp/weapp.ts
@@ -12,13 +12,16 @@ JPage({
   },
   onLoad() {
     console.log('onload', this);
-    this.onShareAppMessage = () => ({
-      title: '改-自定义转发标题',
-      path: '/page/index/index?id=123'
-    });
     setTimeout(() => {
-      this.$router.push('/pages/index/index');
+      this.onShareAppMessage = () => ({
+        title: '改-自定义转发标题',
+        path: '/page/index/index?id=123'
+      });
     }, 100);
+
+    // setTimeout(() => {
+    //   this.$router.push('/pages/index/index');
+    // }, 100);
 
     this.$on('onUnload', () => {
       console.log('weapp-onuload');
