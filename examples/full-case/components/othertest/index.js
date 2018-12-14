534b328c38236269c0f620b13c7680ce7a2c7e03
蒋瑶 <landn172@gmail.com>
2018-12-14 17:52:36 +0800

    feat(JPage JComponent 增加生命周期兼容):

--- examples/full-case/components/othertest/index.js
+++ examples/full-case/components/othertest/index.js
@@ -1,5 +1,46 @@
-Component({
+import { JComponent } from 'jgb-weapp'
+
+JComponent({
+  pageLifetimes: {
+    show() {
+      console.log(`Component show`, this)
+    },
+    hide() {
+      console.log(`Component hide`)
+    }
+  },
+  created() {
+    console.log(`Component created`, this)
+  },
   attached() {
-    console.log(`test component attached`)
+    console.log(`Component attached`)
+  },
+  ready() {
+    console.log(`Component ready`)
+  },
+  test() {
+    console.log(`invoke test`)
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    func() {
+      console.log(`func`)
+    }
   }
 })
--- examples/full-case/src/pages/useplugin/useplugin.js
+++ examples/full-case/src/pages/useplugin/useplugin.js
@@ -1,3 +1,10 @@
-Page({
-  
+import { JPage } from 'jgb-weapp'
+
+JPage({
+  onLoad() {
+    console.info(`Page onLoad`)
+  },
+  onShow() {
+    console.info(`Page onShow`)
+  }
 })
