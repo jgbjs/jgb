ddccbce4b92c30396d1f4cff3bf2abf38f12393d
蒋瑶 <landn172@gmail.com>
2019-02-15 18:07:50 +0800

    feat(jgb-weapp): 增加setData performance

--- packages/jgb-weapp/src/plugins/index.ts
+++ packages/jgb-weapp/src/plugins/index.ts
@@ -1,5 +1,7 @@
 import NativeApiPlugin from './nativeApi';
 import RouterPlugin from './router';
+import SetDataPlugin from './setData';
 import use from './use';
 
-export { use, RouterPlugin, NativeApiPlugin };
+export { use, RouterPlugin, NativeApiPlugin, SetDataPlugin };
+
--- packages/jgb-weapp/src/utils/nextTick.ts
+++ packages/jgb-weapp/src/utils/nextTick.ts
@@ -1,7 +1,12 @@
 // tslint:disable-next-line:ban-types
 export default function(fn: Function, ...args: any[]) {
   fn = typeof fn === 'function' ? fn.bind(null, ...args) : fn;
+  const p = Promise.resolve();
   // @ts-ignore
-  const timerFunc = wx.nextTick ? wx.nextTick : setTimeout;
+  const timerFunc = wx.nextTick
+    ? wx.nextTick
+    : (fn: any) => {
+        p.then(fn);
+      };
   timerFunc(fn);
 }
