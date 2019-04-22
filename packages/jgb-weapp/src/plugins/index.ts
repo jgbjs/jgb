fa0d31ccdb4fc16d17a1fdeac54d168781b9a173
蒋瑶 <landn172@gmail.com>
2019-04-22 15:30:21 +0800

    chore: remove jgb-weapp

--- packages/jgb-weapp/src/plugins/index.ts
+++
@@ -1,8 +0,0 @@
-import ComputedPlugin from './computed';
-import NativeApiPlugin from './nativeApi';
-import RouterPlugin from './router';
-import SetDataPlugin from './setData';
-import use from './use';
-
-export { use, RouterPlugin, NativeApiPlugin, SetDataPlugin, ComputedPlugin };
-
--- packages/jgb-weapp/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@tuhu/miniprogram-api-typings" />
-import * as JGB from './jgb';
-
-export = JGB;
-
-export as namespace JGB;
-
